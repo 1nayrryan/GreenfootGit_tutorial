483639d66913b24e862782c4b48c4553874eb6a8
1nayrryan <155669762+1nayrryan@users.noreply.github.com>
2024-05-17 12:49:48 -0400

    commit

--- balloon.java
+++ balloon.java
@@ -1,23 +1,20 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class snake here.
+ * Write a description of class balloon here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class snake extends Actor
+public class balloon extends Actor
 {
     /**
-     * Act - do whatever the snake wants to do. This method is called whenever
+     * Act - do whatever the balloon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        move(3);
-        //partener made a change
-        if(isAtEdge()){
-            turn(45);
-        }
+        // Add your action code here.
+        turn(45);
     }
 }
